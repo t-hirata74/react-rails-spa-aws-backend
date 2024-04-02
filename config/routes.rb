9964efac7194d8a3d2c58cd2564bc3bcdcd0e2ae
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-02 16:11:11 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ヘルスチェック
+  get '/health_check', to: 'health_checks#index'
+  # 記事一覧
+  get '/blogs', to: 'blogs#index'
+  # 記事詳細  
+  get '/blogs/:id', to: 'blogs#show'
+  # 記事作成
+  post '/blogs', to: 'blogs#create'
 end
